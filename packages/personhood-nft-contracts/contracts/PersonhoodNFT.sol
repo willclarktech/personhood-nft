8a6c2334988ae9ac661d426f61693900a2616ba6
willclarktech <willclarktech@users.noreply.github.com>
2019-12-18 17:06:38 +0100

    :seedling: contracts: Add basic identify function to PersonhoodNFT

--- packages/personhood-nft-contracts/contracts/PersonhoodNFT.sol
+++ packages/personhood-nft-contracts/contracts/PersonhoodNFT.sol
@@ -1,8 +1,22 @@
 pragma solidity >=0.5.12 <0.6.0;
 
+import "@openzeppelin/contracts/drafts/Counters.sol";
 import '@openzeppelin/contracts/ownership/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC721/ERC721Full.sol';
 
 contract PersonhoodNFT is ERC721Full, Ownable {
+	using Counters for Counters.Counter;
+
+	Counters.Counter private _tokenIds;
+
 	constructor() ERC721Full("PersonhoodNFT", "PNFT") public {}
+
+	function identify(address person) external returns (uint256) {
+		_tokenIds.increment();
+
+		uint256 newItemId = _tokenIds.current();
+		_mint(person, newItemId);
+
+		return newItemId;
+	}
 }
