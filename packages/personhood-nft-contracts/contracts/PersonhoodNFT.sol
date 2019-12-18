6c3630ca0ead86ce254fe928548d5a8226d638e3
willclarktech <willclarktech@users.noreply.github.com>
2019-12-18 22:24:52 +0100

    :seedling: contracts: Add Personhood struct to identify function

--- packages/personhood-nft-contracts/contracts/PersonhoodNFT.sol
+++ packages/personhood-nft-contracts/contracts/PersonhoodNFT.sol
@@ -7,16 +7,28 @@ import '@openzeppelin/contracts/token/ERC721/ERC721Full.sol';
 contract PersonhoodNFT is ERC721Full, Ownable {
 	using Counters for Counters.Counter;
 
+	struct Personhood {
+		address issuer;
+		uint256 timestamp;
+	}
+
 	Counters.Counter private _tokenIds;
+	mapping (uint256 => Personhood) private _personhoods;
 
 	constructor() ERC721Full("PersonhoodNFT", "PNFT") public {}
 
 	function identify(address person) external returns (uint256) {
 		_tokenIds.increment();
 
-		uint256 newItemId = _tokenIds.current();
-		_mint(person, newItemId);
+		uint256 tokenId = _tokenIds.current();
+		Personhood memory personhood = Personhood({
+			issuer: msg.sender,
+			timestamp: now
+		});
 
-		return newItemId;
+		_mint(person, tokenId);
+		_personhoods[tokenId] = personhood;
+
+		return tokenId;
 	}
 }
