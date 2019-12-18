880f7a2a25e12eb5427c87355dba8f38b5658423
willclarktech <willclarktech@users.noreply.github.com>
2019-12-18 21:29:06 +0100

    :white_check_mark: contracts: Add test for creating tokens

--- packages/personhood-nft-contracts/test/PersonhoodNFT.test.js
+++ packages/personhood-nft-contracts/test/PersonhoodNFT.test.js
@@ -1,9 +1,31 @@
 const PersonhoodNFT = artifacts.require("PersonhoodNFT");
 
-contract("PersonhoodNFT", () => {
+contract("PersonhoodNFT", ([rootAccount, defaultIdentifier, defaultPerson]) => {
 	it("deploys successfully", async () => {
 		const instance = await PersonhoodNFT.deployed();
 		const name = await instance.name();
 		expect(name).to.equal("PersonhoodNFT");
+	});
+
+	it("allows any account to create tokens for another account", async () => {
+		const instance = await PersonhoodNFT.deployed();
+		const ownedTokensBefore = (
+			await instance.balanceOf(defaultPerson)
+		).toNumber();
+
+		const result = await instance.identify(defaultPerson, {
+			from: defaultIdentifier
+		});
+		const tokenId = result.logs[0].args.tokenId.toNumber();
+
+		const ownedTokensAfter = (
+			await instance.balanceOf(defaultPerson)
+		).toNumber();
+		expect(ownedTokensAfter).to.equal(ownedTokensBefore + 1);
+
+		const retrievedTokenId = (
+			await instance.tokenOfOwnerByIndex(defaultPerson, ownedTokensAfter - 1)
+		).toNumber();
+		expect(retrievedTokenId).to.equal(tokenId);
 	});
 });
