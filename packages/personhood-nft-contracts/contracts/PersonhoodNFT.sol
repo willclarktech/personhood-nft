7276634016ed7c14795e0a20ca30279e411a6822
willclarktech <willclarktech@users.noreply.github.com>
2019-12-18 23:26:13 +0100

    :nail_care: contracts: Remove trailing underscores from private variables

--- packages/personhood-nft-contracts/contracts/PersonhoodNFT.sol
+++ packages/personhood-nft-contracts/contracts/PersonhoodNFT.sol
@@ -13,36 +13,36 @@ contract PersonhoodNFT is ERC721Full, ERC721Burnable, Ownable {
 		uint256 timestamp;
 	}
 
-	Counters.Counter private _tokenIds;
-	mapping (uint256 => Personhood) private _personhoods;
+	Counters.Counter private tokenIds;
+	mapping (uint256 => Personhood) private personhoods;
 
 	event Spend(uint256 tokenId, address recipient, bytes32 memo);
 
 	constructor() ERC721Full("PersonhoodNFT", "PNFT") public {}
 
 	function identify(address person) external returns (uint256) {
-		_tokenIds.increment();
+		tokenIds.increment();
 
-		uint256 tokenId = _tokenIds.current();
+		uint256 tokenId = tokenIds.current();
 		Personhood memory personhood = Personhood({
 			issuer: msg.sender,
 			timestamp: now
 		});
 
 		_mint(person, tokenId);
-		_personhoods[tokenId] = personhood;
+		personhoods[tokenId] = personhood;
 
 		return tokenId;
 	}
 
 	function get(uint256 tokenId) external view returns (address issuer, uint256 timestamp) {
-		Personhood memory personhood = _personhoods[tokenId];
+		Personhood memory personhood = personhoods[tokenId];
 		return (personhood.issuer, personhood.timestamp);
 	}
 
 	function spend(uint256 tokenId, address recipient, bytes32 memo) external {
 		burn(tokenId);
-		delete _personhoods[tokenId];
+		delete personhoods[tokenId];
 		emit Spend(tokenId, recipient, memo);
 	}
 }
