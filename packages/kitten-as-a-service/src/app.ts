eb04aecb90e3b654ad88f3981f95b30926ac373a
willclarktech <willclarktech@users.noreply.github.com>
2019-12-19 12:30:17 +0100

    :seedling: kitten: Serve kitten text with dummy hasPaid middleware

--- packages/kitten-as-a-service/src/app.ts
+++ packages/kitten-as-a-service/src/app.ts
@@ -1,7 +1,16 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
+
+const hasPaid: RequestHandler = (req, res, next) => {
+	next();
+};
+const serveText: RequestHandler = (req, res, next) => {
+	res.send("have a kitten");
+	next();
+};
+app.use("/kitten", hasPaid, serveText);
 
 export default app;
