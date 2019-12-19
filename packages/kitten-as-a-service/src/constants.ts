34f11530c0b7fdb1c9e35035962b1134c22d2b00
willclarktech <willclarktech@users.noreply.github.com>
2019-12-19 20:48:41 +0100

    :seedling: kitten: Read more env variables if set

--- packages/kitten-as-a-service/src/constants.ts
+++ packages/kitten-as-a-service/src/constants.ts
@@ -2,7 +2,7 @@ export const oneHour = 3600000;
 export const spendEventTopic =
 	"0x5d578c853f453cdb9f2bd571dfa428d03e9112e36198626fd6954657625463e7";
 
-// TODO: Set these dynamically
-export const sessionSecret = "keyboard cat";
-export const ethProvider = "ws://localhost:8545";
+// TODO: Remove defaults
+export const sessionSecret = process.env.SESSION_SECRET || "keyboard cat";
+export const ethProvider = process.env.ETH_PROVIDER || "ws://localhost:8545";
 export const port = process.env.PORT || 3000;
