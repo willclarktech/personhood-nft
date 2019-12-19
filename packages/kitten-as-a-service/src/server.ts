8b0f4efb36b07cf7fecfe028678b024596ee1f9a
willclarktech <willclarktech@users.noreply.github.com>
2019-12-19 20:47:04 +0100

    :nail_care: kitten: Remove indentation from server console messages

--- packages/kitten-as-a-service/src/server.ts
+++ packages/kitten-as-a-service/src/server.ts
@@ -4,8 +4,8 @@ const port = app.get("port");
 const env = app.get("env");
 
 const server = app.listen(app.get("port"), () => {
-	console.info(`  App is running at http://localhost:${port} in ${env} mode`);
-	console.info("  Press CTRL-C to stop\n");
+	console.info(`App is running at http://localhost:${port} in ${env} mode`);
+	console.info("Press CTRL-C to stop\n");
 });
 
 export default server;
