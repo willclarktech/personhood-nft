2f74261a9edeb2e195c4d2945df0592e40370358
willclarktech <willclarktech@users.noreply.github.com>
2019-12-19 13:24:11 +0100

    :seedling: kitten: Add dummy session setting

--- packages/kitten-as-a-service/src/app.ts
+++ packages/kitten-as-a-service/src/app.ts
@@ -1,15 +1,34 @@
 import express, { RequestHandler } from "express";
+import session from "express-session";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+app.use(
+	session({
+		secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			maxAge: 3600000
+		}
+	})
+);
+
+const getSession: RequestHandler = (req, res, next) => {
+	if (req.session) req.session.seen = true;
+	res.send("here you go");
+};
+app.get("/session", getSession);
+
 const hasPaid: RequestHandler = (req, res, next) => {
+	if (req.session?.seen) req.session.paid = true;
 	next();
 };
 const serveText: RequestHandler = (req, res, next) => {
-	res.send("have a kitten");
-	next();
+	const text = req.session?.paid ? "have a kitten" : "you don’t get a kitten";
+	res.send(text);
 };
 app.use("/kitten", hasPaid, serveText);
 
