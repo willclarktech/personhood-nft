600d644b60639107eb98c8209b3a75b30cc03b97
willclarktech <willclarktech@users.noreply.github.com>
2019-12-20 15:24:40 +0000

    :house_with_garden: contracts: Override eslint rule in truffle config

--- packages/personhood-nft-contracts/truffle-config.js
+++ packages/personhood-nft-contracts/truffle-config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
