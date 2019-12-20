dd0753b768a95e4ea38adbe62a7fda16943996e0
willclarktech <willclarktech@users.noreply.github.com>
2019-12-20 12:53:59 +0000

    :recycle: kitten: Be more DRY in server

--- packages/kitten-as-a-service/src/server.ts
+++ packages/kitten-as-a-service/src/server.ts
@@ -3,7 +3,7 @@ import app from "./app";
 const port = app.get("port");
 const env = app.get("env");
 
-const server = app.listen(app.get("port"), () => {
+const server = app.listen(port, () => {
 	console.info(`App is running at http://localhost:${port} in ${env} mode`);
 	console.info("Press CTRL-C to stop\n");
 });
