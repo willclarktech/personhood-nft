53d8827fe63906f555c24d0f78759550b6f29844
willclarktech <willclarktech@users.noreply.github.com>
2019-12-20 13:36:32 +0000

    :seedling: kitten: Use market API to update session value

--- packages/kitten-as-a-service/src/handlers.ts
+++ packages/kitten-as-a-service/src/handlers.ts
@@ -3,7 +3,13 @@ import { RequestHandler } from "express";
 import { readFileSync } from "fs";
 import path from "path";
 import Web3 from "web3";
+import { Log } from "web3-core";
 import { address, minimumValue } from "./constants";
+import marketClient from "./market-client";
+
+const encodeHex = (hexString: string) => `0x${hexString}`;
+const encodeAddress = (hexString: string) => encodeHex(hexString.slice(24));
+const decodeHeight = (hexString: string) => parseInt(hexString, 16);
 
 export const getChallenge: (
 	web3: Web3,
@@ -28,37 +34,54 @@ export const getChallenge: (
 		clearTimeout(timeout);
 		if (req.session) req.session.challenge = undefined;
 	};
+
 	const errorHandler = (error: Error) => {
 		console.error(error);
 		clearChallenge();
 	};
+	const dataHandler = async ({ data }: Log) => {
+		const { session } = req;
+		if (!session) {
+			errorHandler(new Error("session not found"));
+			return;
+		}
 
-	subscription
-		.on("data", ({ data }) => {
-			const { session } = req;
-			if (!session) {
-				errorHandler(new Error("session not found"));
-				return;
+		// const prefix = data.slice(0, 2);
+		const tokenIdBytes = data.slice(2, 66);
+		const tokenId = encodeHex(tokenIdBytes);
+		const issuerBytes = data.slice(66, 130);
+		const issuer = encodeAddress(issuerBytes);
+		const heightBytes = data.slice(130, 194);
+		const height = decodeHeight(heightBytes);
+		const recipientBytes = data.slice(194, 258);
+		const recipientAddress = encodeAddress(recipientBytes);
+		const memo = data.slice(258, 322);
+
+		if (recipientAddress === address.toLowerCase() && memo === challenge) {
+			try {
+				const { data: marketData } = await marketClient.get("/rate", {
+					params: {
+						tokenId,
+						issuer,
+						height
+					}
+				});
+				if (typeof marketData.rate !== "number") {
+					throw new Error("unrecognised response from market API");
+				}
+				session!.value += marketData.rate;
+			} catch (error) {
+				console.error(error.message || error.code || "unknown error");
 			}
+		}
 
-			// const prefix = data.slice(0, 2);
-			// const tokenId = data.slice(2, 66);
-			// const issuer = data.slice(66, 130);
-			// const height = data.slice(130, 194);
-			const recipientBytes = data.slice(194, 258);
-			const recipientAddress = `0x${recipientBytes.slice(24)}`;
-			const memo = data.slice(258, 322);
+		if (session!.value >= minimumValue) {
+			challenges.add(challenge);
+			clearChallenge();
+		}
+	};
 
-			if (recipientAddress === address.toLowerCase() && memo === challenge) {
-				session!.value += 0.1;
-			}
-
-			if (session!.value >= minimumValue) {
-				challenges.add(challenge);
-				clearChallenge();
-			}
-		})
-		.on("error", errorHandler);
+	subscription.on("data", dataHandler).on("error", errorHandler);
 };
 
 export const serveText: RequestHandler = (req, res) => {
