95a33d17bb31f697b115ab696c00f7eedc30ffdd
willclarktech <willclarktech@users.noreply.github.com>
2019-12-20 11:21:38 +0000

    :recycle: contracts: Reorder accounts in integration test

--- packages/personhood-nft-contracts/test/integration.test.js
+++ packages/personhood-nft-contracts/test/integration.test.js
@@ -9,56 +9,53 @@ const webServiceClient = axios.create({
 
 const encodeHex = hexString => `0x${hexString}`;
 
-contract(
-	"PersonhoodNFT integration",
-	([_rootAccount, defaultIssuer, defaultPerson, defaultService]) => {
-		it("integrates with a service", async () => {
-			const instance = await PersonhoodNFT.deployed();
+contract("PersonhoodNFT integration", ([_root, person, service, issuer]) => {
+	it("integrates with a service", async () => {
+		const instance = await PersonhoodNFT.deployed();
 
-			// No session -> no kitten
-			try {
-				await webServiceClient.get("/kitten");
-				fail("unexpected successful kitten get");
-			} catch (error) {
-				expect(error.response.status).to.equal(401);
-				expect(error.response.data).to.equal(
-					"visit /challenge to get a challenge"
-				);
-			}
+		// No session -> no kitten
+		try {
+			await webServiceClient.get("/kitten");
+			fail("unexpected successful kitten get");
+		} catch (error) {
+			expect(error.response.status).to.equal(401);
+			expect(error.response.data).to.equal(
+				"visit /challenge to get a challenge"
+			);
+		}
 
-			const challengeResponse = await webServiceClient.get("/challenge");
-			const challenge = challengeResponse.data;
-			expect(challenge).to.match(/^[a-f0-9]{64}$/);
+		const challengeResponse = await webServiceClient.get("/challenge");
+		const challenge = challengeResponse.data;
+		expect(challenge).to.match(/^[a-f0-9]{64}$/);
 
-			const cookieToSet = challengeResponse.headers["set-cookie"][0];
-			const cookie = cookieToSet.slice(0, cookieToSet.indexOf(";"));
-			if (process.env.DEBUG) console.debug(cookie);
+		const cookieToSet = challengeResponse.headers["set-cookie"][0];
+		const cookie = cookieToSet.slice(0, cookieToSet.indexOf(";"));
+		if (process.env.DEBUG) console.debug(cookie);
 
-			// No token burned -> no kitten
-			try {
-				await webServiceClient.get("/kitten", {
-					headers: { cookie }
-				});
-				fail("unexpected successful kitten get");
-			} catch (error) {
-				expect(error.response.status).to.equal(401);
-				expect(error.response.data).to.equal("complete the challenge");
-			}
-
-			const issueResult = await instance.issue(defaultPerson, {
-				from: defaultIssuer
-			});
-			const tokenId = issueResult.logs[0].args.tokenId.toNumber();
-
-			await instance.spend(tokenId, defaultService, encodeHex(challenge), {
-				from: defaultPerson
-			});
-
-			// Token burned -> kitten :)
-			const kittenResponse = await webServiceClient.get("/kitten", {
+		// No token burned -> no kitten
+		try {
+			await webServiceClient.get("/kitten", {
 				headers: { cookie }
 			});
-			expect(kittenResponse.data.length).to.be.above(80000);
+			fail("unexpected successful kitten get");
+		} catch (error) {
+			expect(error.response.status).to.equal(401);
+			expect(error.response.data).to.equal("complete the challenge");
+		}
+
+		const issueResult = await instance.issue(person, {
+			from: issuer
 		});
-	}
-);
+		const tokenId = issueResult.logs[0].args.tokenId.toNumber();
+
+		await instance.spend(tokenId, service, encodeHex(challenge), {
+			from: person
+		});
+
+		// Token burned -> kitten :)
+		const kittenResponse = await webServiceClient.get("/kitten", {
+			headers: { cookie }
+		});
+		expect(kittenResponse.data.length).to.be.above(80000);
+	});
+});
