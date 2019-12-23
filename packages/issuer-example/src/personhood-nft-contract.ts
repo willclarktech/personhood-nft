b4fa91ec20450f90b0e3ec0d5d0c8120b359f6c3
willclarktech <willclarktech@users.noreply.github.com>
2019-12-23 16:48:06 +0000

    :nail_care: issuer: Disable lint rule inline

--- packages/issuer-example/src/personhood-nft-contract.ts
+++ packages/issuer-example/src/personhood-nft-contract.ts
@@ -13,6 +13,6 @@ const personhoodNftContract = new web3.eth.Contract(
 	personhoodNFT.abi as AbiItem[],
 	contractAddress,
 	contractOptions,
-) as any;
+) as any; // eslint-disable-line @typescript-eslint/no-explicit-any
 
 export default personhoodNftContract;
