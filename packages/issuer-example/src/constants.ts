61f7a5191c443de5cbfb5e8473a5919fdd351394
willclarktech <willclarktech@users.noreply.github.com>
2019-12-23 16:25:10 +0000

    :seedling: issuer: Allow constants via env variables

--- packages/issuer-example/src/constants.ts
+++ packages/issuer-example/src/constants.ts
@@ -6,11 +6,15 @@ if (!process.env.GRECAPTCHA_SECRET) {
 export const grecaptchaSecret = process.env.GRECAPTCHA_SECRET;
 export const grecaptchaBaseUrl = "https://www.google.com/recaptcha/api";
 
-export const port = process.env.PORT || 3002;
 export const publicPath = path.join(__dirname, "..", "public");
-export const minimumPersonhoodScore = 0.5;
-export const issuerAddress = "0xe11ba2b4d45eaed5996cd0823791e0c93114882d";
-export const contractAddress = "0xcfeb869f69431e42cdb54a4f4f105c19c080a601";
+export const port = process.env.PORT || 3002;
+export const minimumPersonhoodScore = process.env.MINIMUM_PERSONHOOD_SCORE
+	? parseFloat(process.env.MINIMUM_PERSONHOOD_SCORE)
+	: 0.5;
+export const issuerAddress =
+	process.env.ISSUER_ADDRESS || "0xe11ba2b4d45eaed5996cd0823791e0c93114882d";
+export const contractAddress =
+	process.env.CONTRACT_ADDRESS || "0xcfeb869f69431e42cdb54a4f4f105c19c080a601";
 
 export const ethAddressRegExp = /0x[a-f0-9]{40}/i;
 export const ethProvider = process.env.ETH_PROVIDER || "ws://localhost:8545";
