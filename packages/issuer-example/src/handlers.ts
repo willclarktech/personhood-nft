b08af5543893d4a2cbbea3d099a8273e1567e21e
willclarktech <willclarktech@users.noreply.github.com>
2019-12-24 18:19:50 +0000

    :seedling: issuer: Return token ID in query

--- packages/issuer-example/src/handlers.ts
+++ packages/issuer-example/src/handlers.ts
@@ -12,18 +12,21 @@ export const issueToken: RequestHandler = async (req, res) => {
 	}
 
 	try {
-		const { transactionHash } = await personhoodNftContract.methods
-			.issue(address)
-			.send();
+		const {
+			transactionHash,
+			events,
+		} = await personhoodNftContract.methods.issue(address).send();
 
-		if (!transactionHash) {
+		const tokenId = events?.Transfer?.returnValues?.tokenId;
+
+		if (!transactionHash || !tokenId) {
 			console.error("transaction failed");
 			res.redirect(failurePage);
 			return;
 		}
 
 		console.info(`sent transaction: ${transactionHash}`);
-		res.redirect(successPage);
+		res.redirect(`${successPage}?tokenId=${tokenId}`);
 	} catch (error) {
 		console.error(error.message || error.code || "unknown error");
 		res.redirect(failurePage);
