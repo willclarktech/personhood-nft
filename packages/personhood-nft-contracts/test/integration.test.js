ed5d07b531718f29711bb86d23703ea14ee4bbbc
willclarktech <willclarktech@users.noreply.github.com>
2019-12-25 00:37:13 +0000

    :white_check_mark: contracts: Add integration test with issuer service

--- packages/personhood-nft-contracts/test/integration.test.js
+++ packages/personhood-nft-contracts/test/integration.test.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const puppeteer = require("puppeteer");
 
 const PersonhoodNFT = artifacts.require("PersonhoodNFT");
 
@@ -6,6 +7,8 @@ const webServicePort = process.env.WEB_SERVICE_PORT || 3000;
 const webServiceClient = axios.create({
 	baseURL: `http://localhost:${webServicePort}`,
 });
+
+const issuerPort = process.env.ISSUER_PORT || 3002;
 
 const encodeHex = hexString => `0x${hexString}`;
 
@@ -131,6 +134,52 @@ contract(
 			});
 			const tokenId3 = issueResult3.logs[0].args.tokenId.toNumber();
 			await instance.spend(tokenId3, service, encodeHex(challenge), {
+				from: person,
+			});
+
+			// Token burned -> kitten :)
+			const kittenResponse = await webServiceClient.get("/kitten", {
+				headers: { cookie },
+			});
+			if (!kittenResponse.data) {
+				expect.fail("no response data");
+			}
+			expect(kittenResponse.data.length).to.be.above(80000);
+		});
+
+		it("integrates with a personhood verification service", async () => {
+			const instance = await PersonhoodNFT.deployed();
+
+			const challengeResponse = await webServiceClient.get("/challenge");
+			const challenge = challengeResponse.data;
+			expect(challenge).to.match(/^[a-f0-9]{64}$/);
+
+			const cookieToSet = challengeResponse.headers["set-cookie"][0];
+			const cookie = cookieToSet.slice(0, cookieToSet.indexOf(";"));
+			if (process.env.DEBUG) console.debug(cookie);
+
+			const browser = await puppeteer.launch();
+
+			const page = await browser.newPage();
+			await page.goto(`http://localhost:${issuerPort}/?address=${person}`);
+			await page.waitFor(1000);
+			await page.click("#form-submit");
+			const pageUrl = page.url();
+			expect(
+				pageUrl.startsWith(
+					`http://localhost:${issuerPort}/success.html?tokenId=`,
+				),
+			).to.be.true;
+
+			await page.waitFor(2000);
+
+			const tokenIdElement = await page.$("#token-id");
+			const tokenId = parseInt(
+				await tokenIdElement.evaluate(node => node.innerText),
+				10,
+			);
+
+			await instance.spend(tokenId, service, encodeHex(challenge), {
 				from: person,
 			});
 
